a03512ce17ecae56d91558afd852e2afde99ce81
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-12-17 22:41:06 -0800

    layout complete.

--- client/src/components/menuNav/NavItem.jsx
+++ client/src/components/menuNav/NavItem.jsx
@@ -5,7 +5,7 @@ export default function NavItem(props) {
     const {title} = props
     return (
         <>
-        <figure className="w-full h-full flex flex-col justify-center items-center gap-1">
+        <figure className="w-full h-full flex flex-col justify-center items-center gap-1 hover:bg-success hover:text-white rounded-lg">
           <img id="" alt="" src={FoodAlt} className="w-4/5 h-4/6 rounded-full flex object-cover" />
           <figcaption className="w-full h-auto flex justify-center items-center">
             <p className="w-full h-auto text-center">{title}</p>
