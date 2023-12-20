6cab81fde14c92d90ce81f6e7fba447b0a7b7ab8
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-12-19 23:59:02 -0800

    more layout changes.

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -8,17 +8,17 @@ export default function Hero() {
     return (
       <>
         <HeroHeader />
-        <main className="w-full h-full absolute z-10 top-0">
+        <main className="w-full h-full absolute z-10 top-0 flex flex-col gap-3">
           <header className="w-full h-1/4 absolute top-1/4 z-20 flex flex-wrap justify-around bg-black bg-opacity-30 p-3">
             <h1 className="w-full h-auto text-3xl text-center text-white shadow shadow-lg self-center">
               Johnson Family Cookbook
             </h1>
-            <article className="w-5/6 h-1/6 flex justify-between  items-center gap-3">
-              <button className="w-full py-3 rounded-lg border text-white shadow shadow-xl">
+            <article className="w-full h-1/6 flex justify-between  items-center gap-3">
+              <button className="w-5/6 py-3 rounded-lg  bg-gradient-to-r from-success to-white  text-black shadow shadow-xl">
                 <Link to="/home">Home</Link>
               </button>
-              <button className="w-full py-3 rounded-lg border text-white shadow shadow-xl">
-                Learn More
+              <button className="w-5/6 py-3 rounded-lg shadow shadow-xl bg-gradient-to-r from-success to-white bg-clip text-">
+                  Learn More
               </button>
             </article>
           </header>
@@ -27,18 +27,18 @@ export default function Hero() {
             id="photoGrid"
             className=" w-full h-full border grid grid-rows-4"
           >
-                <section className='w-full h-full  bg-black flex justify-center'>
-                    <figure className='w-full h-full flex object-cover'>
-                        <img src={food2} className='w-full h-full object-cover'/>
-                    </figure>
+            <section className="w-full h-full  bg-black flex justify-center">
+              <figure className="w-full h-full flex object-cover">
+                <img src={food2} className="w-full h-full object-cover" />
+              </figure>
 
-                    <figure className='w-full h-full flex object-cover'>
-                        <img src={food2} className='w-full h-full object-cover'/>
-                    </figure>
+              <figure className="w-full h-full flex object-cover">
+                <img src={food2} className="w-full h-full object-cover" />
+              </figure>
 
-                    <figure className='w-full h-full flex object-cover'>
-                        <img src={food2} className='w-full h-full object-cover'/>
-                    </figure>
+              <figure className="w-full h-full flex object-cover">
+                <img src={food2} className="w-full h-full object-cover" />
+              </figure>
             </section>
 
             <section className="w-full h-full  bg-black flex justify-center">
