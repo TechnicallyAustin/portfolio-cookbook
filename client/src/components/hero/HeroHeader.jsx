d1b30b0b27a3f401c81ed3c3caf1c67179ef7599
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-12-24 22:11:27 -0800

    adjusting header

--- client/src/components/hero/HeroHeader.jsx
+++ client/src/components/hero/HeroHeader.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function HeroHeader() {
   return (
-    <header className="fixed bg-gradient-to-r from-success to-black text-white z-50 w-full h-16 flex justify-center items-center shadow shadow-md shadow-white">
+    <header className="fixed bg-gradient-to-r from-success to-black text-white z-50 w-full h-16 flex justify-center items-center shadow shadow-md">
       <section className="w-full h-full flex justify-between items-center px-3">
         <Link to="/">
           <article className="">Johnson Cookbook</article>
